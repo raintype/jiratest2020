84489d6816e41a9c128757aced572b9997bca198
raintype <raintype@nexon.co.kr>
2020-12-02 14:01:58 +0900

    'test'

--- jiratest.py
+++ jiratest.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', methods = ['POST', 'GET'])
+def index(request):
+    if request.method == 'POST':
+        return 'POST'
+
     return 'Hello Worlkd!'
 
 if __name__ == '__main__':
