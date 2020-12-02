5cbf044f1efa91e8bca9f89cfcbf04b71bb8ef07
raintype <raintype@nexon.co.kr>
2020-12-02 14:22:29 +0900

    'test'

--- jiratest.py
+++ jiratest.py
@@ -6,19 +6,18 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        params = json.loads(request.get_data(), encoding='utf-8')
+	if request.method == 'POST':
+		params = json.loads(request.get_data(), encoding='utf-8')
+
 		if len(params) == 0:
 			return 'No parameter'
 
 		params_str = ''
 		for key in params.keys():
 			params_str += 'key: {}, value: {}<br>'.format(key, params[key])
-		
+
 		print(params_str)
-
-
-    return 'Hello Worlkd!'
+	return 'Hello Worlkd!'
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
